27e43082d70b55ce1cd10a6accd11b35cf64dbcf
aleksandarkacar <kacar95@gmail.com>
2022-12-15 03:22:47 +0100

    zavrsen 4-ti zadatak (dodat db.php za includovanje za laksi access i postovi su poredjani po datumu)

---
+++ db.php
@@ -0,0 +1,29 @@
+<?php
+    //ovde treba namestiti ime i password baze
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        // set the PDO error mode to exception
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+
+
+    function fetchData($connection, $sql, $fetchAll = false) {
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        if($fetchAll) {
+            return $statement->fetchAll();
+        } 
+        
+        return $statement->fetch();
+    }
+?>
